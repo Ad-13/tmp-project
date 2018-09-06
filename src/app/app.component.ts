b67f2d86e7594408289318032f4618917f0df210
Andrew Krupin <ad.kru13@gmail.com>
2018-09-06 16:37:13 +0300

    styles switched to scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { SelectItem } from 'primeng/api';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
