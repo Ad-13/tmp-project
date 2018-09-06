f8d897920c77e707ebd37327cfec14808cc31787
Andrew Krupin <ad.kru13@gmail.com>
2018-09-06 16:20:27 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import * as moment from 'moment';
+import { SelectItem } from 'primeng/api';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  now = moment().format('LLLL');
+
+  cars: SelectItem[];
+
+  selectedCars1: string[] = [];
+
+  selectedCars2: string[] = [];
+
+  constructor() {
+    this.cars = [
+        {label: 'Audi', value: 'Audi'},
+        {label: 'BMW', value: 'BMW'},
+        {label: 'Fiat', value: 'Fiat'},
+        {label: 'Ford', value: 'Ford'},
+        {label: 'Honda', value: 'Honda'},
+        {label: 'Jaguar', value: 'Jaguar'},
+        {label: 'Mercedes', value: 'Mercedes'},
+        {label: 'Renault', value: 'Renault'},
+        {label: 'VW', value: 'VW'},
+        {label: 'Volvo', value: 'Volvo'}
+    ];
+}
 }
